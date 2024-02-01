7333449cd2e6d3b9edad44238642291849ebda51
Aryan Kumar <119026427+Aryankumar281@users.noreply.github.com>
2024-02-01 09:49:03 +0530

    Add files via upload

---
+++ Drag and Drop/index.js
@@ -0,0 +1,28 @@
+let lists = document.querySelectorAll('.list')
+let right = document.querySelector('.right')
+let left = document.querySelector('.left')
+
+
+lists.forEach(list =>{
+    list.addEventListener('dragstart',(e) => {
+        let selected = e.target;
+
+       
+        right.addEventListener('dragover', (e) =>{
+            e.preventDefault();
+        })
+        right.addEventListener('drop', (e) =>{
+            right.appendChild(selected);
+            selected = null;
+        })
+        
+
+        left.addEventListener('dragover', (e) =>{
+            e.preventDefault();
+        })
+        left.addEventListener('drop', (e) =>{
+            left.appendChild(selected);
+            selected = null;
+        })
+    }) 
+})
